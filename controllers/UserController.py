a45a8afe1b8e62c78c2453f6f7790555a50d6a57
JanzenCode <Jordynsmall@icloud.com>
2022-11-12 19:48:20 +1000

    updated models

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request
 from init import db, ma, bcrypt, jwt
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
 from sqlalchemy.exc import IntegrityError
 from models.users import User, UserSchema
 
 users_bp = Blueprint('users',__name__, url_prefix='/auth')
 
-@users_bp.route('/register/ ')
+@users_bp.route('/register/' methods=['POST'])
 def auth_register():
     try:
         user = User(
@@ -14,9 +16,19 @@ def auth_register():
         password = bcrypt.generate_password_hash(request.json['password']).decode('utf-8'),
         age = request.json('age'),
         )
-
         db.session.add(user)
         db.session.commit()
+
         return UserSchema(exclude = ['password']).dump(user), 201
     except IntegrityError:
         return {'error': 'email is already in use, please use another'}, 409
+
+@users_bp.route('/login/', methods=['POST'])
+def user_login():
+    stmt = db.select(User).filter_by(email=request.json['email'])
+    user = db.session.scalar(stmt)
+    if user and bcrypt.check_password_hash(user.password, request.json['password']):
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        return {'email': user.email, 'token': token}
+    else:
+        return {'error': 'Invalid email or password.'}, 401
